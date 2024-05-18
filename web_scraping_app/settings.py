c210a3d08aabc62df87148d055379edf1c51fd50
priiiya3 <priyasingh3709@gmail.com>
2024-05-18 22:56:49 +0530

    fixing bg issue

--- web_scraping_app/settings.py
+++ web_scraping_app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-ozp7o5z@)c@df&l&v1)k&c=x*frhs$63$j8(%8wja2d2*00%7(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
 
 
 # Application definition
